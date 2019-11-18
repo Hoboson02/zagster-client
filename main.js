65c58d7041e034a7e4f706884416a664dbd8f238
Ryan Earl <ryanleoearl@gmail.com>
2019-11-18 15:33:28 -0800

    Merged both files

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
+const BASE_URL = "https://zagster-service.herokuapp.com"
 $(updateView)
 
 function updateView() {
     alert("You are my guest and I am therefore in your service")
+}
+function updateView() {
+  $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
+}
+
+function updateRideCount(data) {
+  numberOfRides = data.count
+  $("h2#rideCount").html(numberOfRides)
 }
 //You are my guest and I am therefore in your service
 //I am honored, but I have worked a lifetime to be free of servitude.
